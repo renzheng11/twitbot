db93fe86314b36a8b9e489a9066b1e7ae57679c5
renzheng11 <renzheng112@gmail.com>
2021-10-26 10:23:00 -0400

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'vBuaielSsYihQahA5gRZeP8dR',
+  consumer_secret:      'G2XQ7F5X7FxAbtFqH19j8H0lu6COzOShh5uORCX4SDx2x6Lb4z',
+  access_token:         '1452705024820203521-uecNugDVhcq50Q5fSFXuj6Ax6Q7tEt',
+  access_token_secret:  'ApczxKazycJXQL143K3qMn26EAT2end4hg3XwXteMhiW3'
+}
